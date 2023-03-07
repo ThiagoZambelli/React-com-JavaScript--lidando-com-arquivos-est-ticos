bae7504ee374e492f012e90fcce2b23f80680551
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-03-07 14:38:19 -0300

    Criação dos estilos do header com scss (preciso aprender sobre)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './style/estilosGlobais.scss'
 import PaginaInicial from "./paginas/PaginaInicial";
 
 
--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,12 +1,14 @@
 import logo from './logo.png';
 import search from './search.png';
+import style from './Cabecalho.module.scss'
+
 
 export default function Cabecalho(){
     return(
-        <header>
+        <header className={style.cabecalho}>
             <img src={logo} alt="Logo Alura Space" />
-            <div>
-                <input type="search"  placeholder="O que você busca?"/>
+            <div className={style.cabecalho__container}>
+                <input className={style.cabecalho__input} type="search"  placeholder="O que você busca?"/>
                 <img src={search} alt="Icone de Lupa" />
             </div>
         </header>
